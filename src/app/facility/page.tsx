f58ffeb36b2a4de5255f7ed8e1cde0a58101d0fb
VinKnee13 <vincentni_@hotmail.com>
2024-08-06 17:54:07 +1000

    Update page.tsx (#1)

--- src/app/facility/page.tsx
+++ src/app/facility/page.tsx
@@ -5,6 +5,8 @@ export default function facilityManagment() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-around p-24">
             <h1 className="text-4xl">Facility Management Page</h1>
+            <h2 className="text-4xl">Hello World!</h2>
+
         </main>
     );
 }
