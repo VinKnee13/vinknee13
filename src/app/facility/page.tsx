6f5090c55b6bd6794eca564f0b43b9146e20dc9c
VinKnee13 <vincentni_@hotmail.com>
2024-08-10 16:25:02 +1000

    Update page.tsx

--- src/app/facility/page.tsx
+++ src/app/facility/page.tsx
@@ -5,8 +5,86 @@ export default function facilityManagment() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-around p-24">
             <h1 className="text-4xl">Facility Management Page</h1>
-            <h2 className="text-4xl">Hello World!</h2>
-            <h3 className="text-4xl">Can you see this?</h3>
+          
+         
+
+<div className="Table">
+<table style="width:100%">
+  <tr>
+    <th>Room</th>
+    <th>Availablility</th>
+    <th>Resident</th>
+  </tr>
+  
+  <tr>
+    <td>1-A</td>
+    <td>Occupied</td>
+    <td>Alice Banks</td>
+  </tr>
+  
+    <tr>
+    <td>1-A</td>
+    <td>Occupied</td>
+    <td>Arpit Puri</td>
+  </tr>
+  
+    <tr>
+    <td>1-B</td>
+    <td>Occupied</td>
+    <td>Alice Banks</td>
+  </tr>
+  
+    <tr>
+    <td>2-A</td>
+    <td>Occupied</td>
+    <td>Luca Biondi</td>
+    
+  </tr>
+    <tr>
+    <td>2-B</td>
+    <td>Occupied</td>
+    <td>Lucas Griffin</td>
+  </tr>
+  
+    <tr>
+    <td>3-A</td>
+    <td>Occupied</td>
+    <td>Vincent Ni</td>
+  </tr>
+  
+  
+    <tr>
+    <td>3-B</td>
+    <td>Occupied</td>
+    <td>Jun Hatori</td>
+  </tr>
+  
+     <tr>
+    <td>4-A</td>
+    <td>Available</td>
+    <td></td>
+  </tr>
+  
+    <tr>
+    <td>4-B</td>
+    <td>Available</td>
+    <td></td>
+  </tr>
+  
+  
+  
+</table>
+</div>
+
+
+
+
+
+
+
+            
         </main>
     );
 }
+
+
