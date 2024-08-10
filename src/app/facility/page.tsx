505390923db491725a88172814be1bfdb584c597
VinKnee13 <vincentni_@hotmail.com>
2024-08-10 17:55:00 +1000

    Update page.tsx

--- src/app/facility/page.tsx
+++ src/app/facility/page.tsx
@@ -5,7 +5,7 @@ export default function facilityManagment() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-around p-24">
            
-  <h1 className="text-3xl">Rooms<br></h1>
+  <h1 className="text-3xl">Rooms</h1>
 
 <table className="table-auto border-4 border-indigo-500/100">
  <thead>
