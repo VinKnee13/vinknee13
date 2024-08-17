b44e15a5b5b96044b64f2461a4fd478eb9dcac7a
VinKnee13 <vincentni_@hotmail.com>
2024-08-17 14:56:38 +1000

    Create App.js

---
+++ src/app/facility/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import Table from './Table';
+
+function App() {
+  return (
+    <div className="App">
+      <Table />
+    </div>
+  );
+}
+
+export default App;
