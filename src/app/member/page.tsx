f4c705196372a5ffe4c0fd50bcfeb9d8df91db03
VinKnee13 <vincentni_@hotmail.com>
2024-08-17 19:22:53 +1000

    Create page.tsx

---
+++ src/app/member/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function facilityManagment() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-around p-24">
+      <h1 className="text-4xl">Service1</h1>
+    </main>
+  );
+}
